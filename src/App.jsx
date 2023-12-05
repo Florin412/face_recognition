9039761247eec68c1804adead924fb44b1294a29
Lupau Remus <lupau.remus15@gmail.com>
2023-12-05 12:40:26 +0200

    update structure

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        {/* <Navigation></Navigation> */}
+        {/* <Logo></Logo> */}
+        {/* <Rank></Rank> */}
+        {/* <ImageLinkForm></ImageLinkForm> */}
+        {/* <FaceRecognition></FaceRecognition> */}
+      </div>
+    );
+  }
+}
+
+export default App;
