c5f6c383e13a7c6a85786414ff078ad2f531960f
Lupau Remus <lupau.remus15@gmail.com>
2023-12-05 12:20:52 +0200

    folder structure made

---
+++ src/containers/App.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div>Salut</div>;
+  }
+}
+
+export default App;
