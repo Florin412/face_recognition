51a356be00ef71d666cdf4268175beeb1d49aa8a
Lupau Remus <lupau.remus15@gmail.com>
2024-02-13 17:32:13 +0200

    .

--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -1,7 +1,4 @@
 const Rank = ({ name, entries }) => {
-  // Accesați valoarea direct din obiect
-  // const entryValue = entries && entries.entries; // înlocuiți "entries" cu numele exact al cheii din obiectul entries
-  // console.log(entries);
   return (
     <div className="text-center text-white">
       <div className="fs-5">{`${name}, your current entry count is...`}</div>
